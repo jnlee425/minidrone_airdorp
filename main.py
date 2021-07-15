a0b407cdafc727b42d0ca6ce13b7e5e4c31f9a89
jnlee425 <82198267+jnlee425@users.noreply.github.com>
2021-07-15 11:34:38 +0900

    Update main.py

--- main.py
+++ main.py
@@ -414,8 +414,8 @@ try:
                 drone.sendControlPosition16(0, 0, -1, 5, 0, 0) #아래로 이동
                 sleep(1)
             elif cy < 240-50 :
-                    drone.sendControlPosition16(0, 0, 1, 5, 0, 0) #위로 이동
-                    sleep(1)
+                drone.sendControlPosition16(0, 0, 1, 5, 0, 0) #위로 이동
+                sleep(1)
             else :
                 know_center = 1
 
